ca763955ab78381a9f76e50d277b06d6bd7ffaa1
palateo8967 <palateo8967@gmail.com>
2025-05-27 19:07:32 -0300

    me falto el ejemplo de insercion

--- server/test.sql
+++ server/test.sql
@@ -211,3 +211,18 @@ CREATE TABLE requeriments(
     almacenamiento VARCHAR(255), 
     FOREIGN KEY (id_game) REFERENCES games(id_game) 
  );
+
+ --ejemplo de insercion
+ -- Insertar requerimientos mínimos
+INSERT INTO requeriments (
+    id_game, tipo, procesador, memoria, graficos, almacenamiento
+) VALUES (
+    7, 'minimos', 'Intel Core 2 Duo E5200', '4 GB RAM', 'GeForce 9800GTX+ (1GB)', '9 GB disponibles'
+);
+
+-- Insertar requerimientos recomendados
+INSERT INTO requeriments (
+    id_game, tipo, procesador, memoria, graficos, almacenamiento
+) VALUES (
+    7, 'recomendados', 'Intel Core i5', '8 GB RAM', 'GeForce GTX 560', '9 GB disponibles'
+);
