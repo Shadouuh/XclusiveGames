82f27b2a926ec1f0fcd6c3d60590c7743313c767
Juanerros <juancottier0@gmail.com>
2025-06-08 21:22:28 -0300

    Axios baseUrl

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL: 'http://localhost:5001', // cuando trabajen en localhost
+  //baseURL: 'http://10.0.1.194:5001', // cuando trabajen en la red local pongan la ip de su compu y el puerto sigue siendo el mismo
   headers: {
     'Content-Type': 'application/json'
   },
